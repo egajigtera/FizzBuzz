b5a6d547398395e96b6e6078d1f4d3a338fc328a
siqiw <siqiw.wang@mail.utoronto.ca>
2023-09-13 23:54:50 -0400

    Multiples for makeuss

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,12 +1,12 @@
 public class Multiples {
-    public static void main(String[] args) {
+    public static int main(int n, int a, int b) {
         int count;
         count = 0;
-        for(int i = 1; i < 1000; i++){
-            if (i % 3 == 0 || i % 5 == 0){
+        for(int i = 1; i < n; i++){
+            if (i % a == 0 || i % b == 0){
                 count++;
             }
         }
-        System.out.println(count);
+        return(count);
     }
 }
